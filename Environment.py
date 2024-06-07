85dbf5fd5444ce85d53dd8963fb957543451a950
Slinking196 <matias.bugueno.b@mail.pucv.cl>
2024-06-06 22:54:03 -0400

    finish a env version

--- Environment.py
+++ Environment.py
@@ -2,30 +2,17 @@ import numpy as np
 import pandas as pd
 import pandas_ta as ta
 from enum import Enum
-
-class STATES(Enum):
-    IN_LONG = 1
-    IN_SHORT = 2
-    WAITING = 3
-
-class ACTIONS(Enum):
-    OPEN_LONG = 1
-    OPEN_SHORT = 2
-    CLOSE_POS = 3
-    NONE = 4
+from utils import STATES, ACTIONS
 
 class Environment():
     def __init__(self, file_name: str, range_time: int = 5, n_sma1: int = 10, n_sma2: int = 20, n_rsi: int = 15) -> None:
         self.data = self.__prepare_init_data__(file_name, range_time, n_sma1, n_sma2, n_rsi)
-        self.money = 1          # Entorno
-        self.entry_price = 0    # Entorno
-        self.exit_price = 0     # Entorno
-        self.max_long = 0       # Estrategia
-        self.min_short = 0      # Estrategia
-        self.stop_win = 0       # Estrategia
-        self.trading_state = STATES.WAITING # Entorno
-        self.time = 0           # Entorno
-        self.historial_capital = [] # Entorno
+        self.fee_ratio = 0.0005
+        self.money = 1          
+        self.entry_price = 0    
+        self.exit_price = 0     
+        self.trading_state = STATES.WAITING 
+        self.time = 0           
 
     def __prepare_init_data__(self, file_name: str, range_time: int, n_sma1: int, n_sma2: int, n_rsi: int):
         df = pd.read_csv(file_name)
@@ -42,28 +29,20 @@ class Environment():
         df_grouped['sma_' + str(n_sma1)] = ta.sma(df['close'], n_sma1)
         df_grouped['sma_' + str(n_sma2)] = ta.sma(df['close'], n_sma2)
         df_grouped['rsi_' + str(n_rsi)] = ta.rsi(df['close'], n_rsi)
+        df_cleaned = df_grouped.dropna()
+        df_cleaned.reset_index(drop=True, inplace=True)
 
-        print(df_grouped.head(50))
-
-        return df_grouped
+        return df_cleaned
     
     def reset(self) -> None:
         self.money = 1
         self.entry_price = 0
         self.exit_price = 0
-        self.max_long = 0
-        self.min_short = 0
-        self.stop_win = 0
         self.trading_state = STATES.WAITING
         self.time = 0
-        self.historial_capital = []
 
-    def step(self, actions: np.ndarray, x: np.ndarray):
+    def step(self, actions: np.ndarray) -> tuple:
         act = np.argmax(actions)
-        stop_long = x[-4]           # Estrategia
-        stop_short = x[-3]          # Estrategia
-        take_profit_long = x[-2]    # Estrategia
-        take_profit_short = x[-1]   # Estrategia
 
         state = self.data.iloc[self.time]
         rewards = 0
@@ -72,53 +51,33 @@ class Environment():
         if self.trading_state == STATES.WAITING:
             if act == ACTIONS.OPEN_LONG:
                 self.entry_price =  state['close']
-                self.max_long = state['close']
-                self.stop_win = self.entry_price * take_profit_long
                 self.trading_state = STATES.IN_LONG
 
             if act == ACTIONS.OPEN_SHORT:
-                self.entry_price =  state['open']
-                self.min_short = state['open']
-                self.stop_win = self.entry_price * take_profit_short
+                self.entry_price =  state['close']
                 self.trading_state = STATES.IN_SHORT
 
-        elif self.trading_state == STATES.IN_LONG:
-            if state['open'] > self.max_long: self.max_long = state['open']
+        elif act == ACTIONS.CLOSE_POS:
+            self.exit_price = state['close']
 
-            if (state['open'] < self.max_long * stop_long) or (state['open'] > self.stop_win):
-                # Calculo de ganancia
-                self.exit_price = state['open']
-                rewards = self.exit_price - self.entry_price
-                
-                end = True
-                self.money += rewards
-                self.trading_state = STATES.WAITING
+            if self.trading_state == STATES.IN_LONG: gross_pnl = ((1 / self.entry_price) - (1 / self.exit_price))
+            elif self.trading_state == STATES.IN_SHORT: gross_pnl = ((1 / self.exit_price) - (1 / self.entry_price))
 
-        elif self.trading_state == STATES.IN_SHORT:
-            if state['open'] < self.min_short: self.min_short = state['price']
+            entry_fee = self.fee_ratio * (1 / self.entry_price)
+            exit_fee = self.fee_ratio * (1 / self.exit_price)
+            funding_fee = self.fee_ratio * self.entry_price
+            fee_paid = entry_fee + exit_fee + funding_fee
 
-            if (state['open'] > self.min_short * stop_short) or (state['price'] < self.stop_win):
-                self.exit_price = state['open']
-                rewards = self.entry_price - self.exit_price
-
-                end = True
-                self.money += rewards
-                self.trading_state = STATES.WAITING
-
-        self.historial_capital.append((self.money, self.time))
+            rewards = gross_pnl - (fee_paid)
+            end = True
 
         self.time += 1
         new_state = self.data.iloc[self.time]
 
         return rewards, new_state, end
     
-    def get_historial(self) -> np.ndarray:
-        return np.array(self.historial_capital)
-    
     def get_money(self) -> float:
         return self.money
     
     def get_traiding_state(self) -> STATES:
         return self.trading_state
-                
-env = Environment('./dataset.csv')
