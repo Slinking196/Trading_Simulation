a8a75d56968cc88f720f2287021895f7c030c1d9
Slinking196 <matias.bugueno.b@mail.pucv.cl>
2024-07-12 21:24:56 -0400

    change prepare init_data to use data adapter

--- trading/Environment.py
+++ trading/Environment.py
@@ -1,12 +1,13 @@
 import numpy as np
 import pandas as pd
 import pandas_ta as ta
-from enum import Enum
 from trading.utils import BET_STATES, ACTIONS
+from trading.adapter import DataAdapter
 
 class Environment():
-    def __init__(self, file_name: str, range_time: int = 5, n_sma1: int = 10, n_sma2: int = 20, n_rsi: int = 15) -> None:
-        self.__data = self.__prepare_init_data__(file_name, range_time, n_sma1, n_sma2, n_rsi)
+    def __init__(self, file_name: str, data_adapter: DataAdapter) -> None:
+        self.__data_adapter = data_adapter
+        self.__data = self.__prepare_init_data__(file_name)
         self.__fee_ratio = 0.0005
         self.__money = 1          
         self.__entry_price = 0    
@@ -17,28 +18,10 @@ class Environment():
 
         self.__time = 0          
 
-    def __prepare_init_data__(self, file_name: str, range_time: int, n_sma1: int, n_sma2: int, n_atr: int):
+    def __prepare_init_data__(self, file_name: str):
         df = pd.read_csv(file_name)
 
-        df_grouped = df.groupby(df.index // range_time).agg({
-                        'open_time': 'first',
-                        'open': 'first',
-                        'high': 'max',
-                        'low': 'min',
-                        'close': 'last',
-                        'volume': 'sum'
-                    }).reset_index(drop=True)
-
-        df_grouped['atr' + str(n_atr)] = ta.atr(df['high'], df['low'], df['close'], n_atr)
-        df_grouped['sma_' + str(n_sma1)] = ta.sma(df['close'], n_sma1)
-        df_grouped['sma_' + str(n_sma2)] = ta.sma(df['close'], n_sma2)
-        df_grouped['sma_' + str(n_sma1) + '_prev'] = df_grouped['sma_' + str(n_sma1)].shift(1)
-        df_grouped['sma_' + str(n_sma2) + '_prev'] = df_grouped['sma_' + str(n_sma2)].shift(1)
-        df_cleaned = df_grouped.dropna()
-        df_cleaned = df_cleaned.drop(columns= ['volume'])
-        df_cleaned.reset_index(drop=True, inplace=True)
-
-        return df_cleaned
+        return self.__data_adapter.get_history(df)
     
     def reset(self) -> None:
         self.__money = 1
@@ -84,10 +67,10 @@ class Environment():
             (self.__trading_state == BET_STATES.IN_SHORT and self.__reference_point > state['close']):
             self.__reference_point = state['close']
 
-        self.__time += 1
-        if self.__time == self.__data.shape[0]:
+        if self.__time + 1 == self.__data.shape[0]:
             return rewards, state, True
 
+        self.__time += 1
         new_state = self.__data.iloc[self.__time]
         new_state = pd.concat([new_state, pd.Series([self.__reference_point, self.__trading_state.value], index=['reference_point', 'state'])])
 
